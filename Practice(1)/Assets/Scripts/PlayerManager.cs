2eff13a82dd298763c46122f43d94f950d9f188c
WonJongHyeok <63942325+WonJongHyeok@users.noreply.github.com>
2020-09-27 17:57:43 +0900

    1차푸시
    1

---
+++ Practice(1)/Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    public int PlayerMXHp;
+    public int currentHp;
+    public float speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Practice(1)/Assets/Scripts/buttonmanager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buttonmanager : MonoBehaviour
+{
+    public void Attack()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
